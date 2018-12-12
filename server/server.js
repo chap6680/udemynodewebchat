3eea4f0756cfed7dc5897b5089a7508a2d35e49c
david chapman <chap6680@gmail.com>
2018-12-12 08:12:26 -0700

    initial

---
+++ server/server.js
@@ -0,0 +1,16 @@
+
+const path = require('path');
+const express = require('express');
+const publicpath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+console.log(publicpath);
+
+var app = express();
+
+app.use(express.static(publicpath));
+
+
+app.listen(port, () => {
+	console.log(`Server started on {port}`);
+});
